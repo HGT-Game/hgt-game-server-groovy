3e7748d6081e19f0e33dd60de01080ccdecbcac4
hdfg159 <hdfg159@126.com>
2020-10-24 16:57:15 +0800

    调整client代码

--- src/main/groovy/io/github/hdfg159/game/client/GameClient.groovy
+++ src/main/groovy/io/github/hdfg159/game/client/GameClient.groovy
@@ -1,10 +1,12 @@
 package io.github.hdfg159.game.client
 
 import groovy.util.logging.Slf4j
+import io.github.hdfg159.common.util.IdUtils
 import io.github.hdfg159.game.domain.dto.GameMessage
 import io.github.hdfg159.game.enumeration.ProtocolEnums
 import io.github.hdfg159.game.util.GameUtils
 import io.netty.bootstrap.Bootstrap
+import io.netty.channel.Channel
 import io.netty.channel.ChannelFuture
 import io.netty.channel.EventLoopGroup
 import io.netty.channel.nio.NioEventLoopGroup
@@ -24,13 +26,15 @@ import io.netty.channel.socket.nio.NioSocketChannel
 class GameClient {
 	ChannelFuture startFuture
 	EventLoopGroup group
+	def port = 9998
+	def host = "127.0.0.1"
 	
 	void start() {
 		group = new NioEventLoopGroup()
 		def bootstrap = new Bootstrap()
 		bootstrap.group(group)
 				.channel(NioSocketChannel.class)
-				.remoteAddress("127.0.0.1", 9998)
+				.remoteAddress(host, port)
 				.handler(new GameClientChannelInitializer())
 		
 		startFuture = bootstrap.connect().sync()
@@ -49,47 +53,38 @@ class GameClient {
 		
 		// 下面业务代码
 		if (channel.active) {
-			(1..1000).each {
-				def username = UUID.randomUUID().toString()
-				def password = "admin"
-				def reg = GameUtils.reqMsg(ProtocolEnums.REQ_REGISTER,
-						GameMessage.RegisterReq.newBuilder()
-								.setUsername(username)
-								.setPassword(password)
-								.build())
-				channel.writeAndFlush(reg)
-				
-				// def username = "admin"
-				// def password = "admin"
-				// def reg = GameUtils.reqMsg(ProtocolEnums.REQ_REGISTER,
-				// 		GameMessage.RegisterReq.newBuilder()
-				// 				.setUsername(username)
-				// 				.setPassword(password)
-				// 				.build())
-				// channel.writeAndFlush(reg)
-				// Thread.sleep(1000L)
-				// def login = GameUtils.reqMsg(
-				// 		ProtocolEnums.REQ_LOGIN,
-				// 		GameMessage.LoginReq.newBuilder()
-				// 				.setUsername(username)
-				// 				.setPassword(password)
-				// 				.build()
-				// )
-				
-				// (1..9).each {
-				// 	channel.writeAndFlush(login)
-			}
-			
-			// Thread.sleep(3000L)
-			// def test1 = GameUtils.reqMsg(
-			// 		ProtocolEnums.REQ_TEST,
-			// 		GameMessage.TestReq.newBuilder()
-			// 				.setStr("asdasd")
-			// 				.build()
-			// )
-			// channel.writeAndFlush(test1)
-			// }
+			register(channel)
 		}
+	}
+	
+	def static register(Channel channel) {
+		def username = UUID.randomUUID().toString()
+		def password = "admin"
+		def registerReq = GameMessage.RegisterReq.newBuilder()
+				.setUsername(username)
+				.setPassword(password)
+				.build()
+		def reg = GameUtils.reqMsg(ProtocolEnums.REQ_REGISTER, registerReq)
+		channel.writeAndFlush(reg)
+	}
+	
+	def static login(Channel channel) {
+		def login = GameUtils.reqMsg(
+				ProtocolEnums.REQ_LOGIN,
+				GameMessage.LoginReq.newBuilder()
+						.setUsername("admin")
+						.setPassword("admin")
+						.build()
+		)
+		channel.writeAndFlush(login)
+	}
+	
+	def static test(Channel channel) {
+		def testReq = GameMessage.TestReq.newBuilder()
+				.setStr(IdUtils.idStr)
+				.build()
+		def test = GameUtils.reqMsg(ProtocolEnums.REQ_TEST, testReq)
+		channel.writeAndFlush(test)
 	}
 	
 	void stop() {
