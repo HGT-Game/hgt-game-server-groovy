3156c802d254c8d3b07c9f0b59fd42ace3d6de9f
hdfg159 <hdfg159@126.com>
2020-11-02 22:34:09 +0800

    海龟汤-调整启动顺序

--- src/main/groovy/io/github/hdfg159/game/GameVerticle.groovy
+++ src/main/groovy/io/github/hdfg159/game/GameVerticle.groovy
@@ -1,19 +1,12 @@
 package io.github.hdfg159.game
 
-
 import groovy.util.logging.Slf4j
-import io.github.hdfg159.game.config.ServerConfig
-import io.github.hdfg159.game.server.GameServer
 import io.github.hdfg159.game.service.avatar.AvatarService
 import io.github.hdfg159.game.service.farm.FarmService
 import io.github.hdfg159.game.service.soup.TurtleSoupService
 import io.github.hdfg159.scheduler.SchedulerManager
 import io.reactivex.Completable
-import io.vertx.core.json.Json
 import io.vertx.reactivex.core.AbstractVerticle
-
-import static io.github.hdfg159.game.constant.GameConsts.SERVER_CONFIG
-import static io.reactivex.schedulers.Schedulers.io
 
 /**
  * 游戏 verticle
@@ -28,22 +21,9 @@ class GameVerticle extends AbstractVerticle {
 	Completable rxStart() {
 		log.info "deploy ${this.class.simpleName}"
 		
-		this.@vertx.fileSystem()
-				.rxReadFile(SERVER_CONFIG)
-				.map({buffer ->
-					log.info "server config:${Json.decodeValue(buffer.delegate)}"
-					Json.decodeValue(buffer.delegate, ServerConfig.class)
-				})
-				.flatMapCompletable({config ->
-					Completable.fromCallable({
-						GameServer.instance.start(this.@vertx, config)
-					}).subscribeOn(io())
-				})
-				.concatWith(
-						this.@vertx.rxDeployVerticle(AvatarService.getInstance()).ignoreElement()
-								.mergeWith(this.@vertx.rxDeployVerticle(FarmService.getInstance()).ignoreElement())
-								.mergeWith(this.@vertx.rxDeployVerticle(TurtleSoupService.getInstance()).ignoreElement())
-				)
+		this.@vertx.rxDeployVerticle(AvatarService.getInstance()).ignoreElement()
+				.mergeWith(this.@vertx.rxDeployVerticle(FarmService.getInstance()).ignoreElement())
+				.mergeWith(this.@vertx.rxDeployVerticle(TurtleSoupService.getInstance()).ignoreElement())
 	}
 	
 	@Override
@@ -51,11 +31,6 @@ class GameVerticle extends AbstractVerticle {
 		Completable.fromRunnable({
 			SchedulerManager.INSTANCE.shutdown()
 			log.info "shutdown scheduler manager"
-		}).concatWith(
-				Completable.fromAction({
-					log.info "undeploy ${this.class.simpleName}"
-					GameServer.instance.stop()
-				}).subscribeOn(io())
-		)
+		})
 	}
 }
