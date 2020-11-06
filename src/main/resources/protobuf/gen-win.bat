6f5831872f9a39631a687d65bad797d1b61d7693
hdfg159 <hdfg159@126.com>
2020-11-07 00:28:09 +0800

    格式化 proto

--- src/main/resources/protobuf/gen-win.bat
+++
@@ -1,6 +0,0 @@
-C:\Users\admin\Downloads\Compressed\protoc-3.13.0-win64\bin\protoc.exe GameMessage.proto --java_out=..\..\groovy
-C:\Users\admin\Downloads\Compressed\protoc-3.13.0-win64\bin\protoc.exe EventMessage.proto --java_out=..\..\groovy
-C:\Users\admin\Downloads\Compressed\protoc-3.13.0-win64\bin\protoc.exe SoupMessage.proto --java_out=..\..\groovy
-C:\Users\admin\Downloads\Compressed\protoc-3.13.0-win64\bin\protoc.exe SoupEvent.proto --java_out=..\..\groovy
-
-pause
